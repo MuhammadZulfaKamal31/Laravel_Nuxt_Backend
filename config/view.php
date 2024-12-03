066e2747de8eaf345186b86744239b6556386e35
MuhammadZulfaKamal31 <zulfakamal019@gmail.com>
2024-12-03 15:32:04 +0700

    coba lagi

--- config/view.php
+++ config/view.php
@@ -8,8 +8,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be checked for your views. Of course
-    | the usual Laravel view path has already been registered for you.
+    | an array of paths that should be checked for your views. Of coursesdsds
+    | the usual Laravel view path has already been registered for you.sdsd
     |
     */
 
